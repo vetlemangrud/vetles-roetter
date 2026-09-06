65a0b512420a401265f90b2503e852a210dfb315
Vetle Mangrud Refsnes <vetle@refsnes.house>
2026-09-06 19:45:05 +0200

    add missing returns

--- carrot_handler.go
+++ carrot_handler.go
@@ -30,6 +30,7 @@ func (h CarrotHandler) Get(w http.ResponseWriter, r *http.Request) {
 	carrots, err := h.Repository.findCarrots()
 	if err != nil {
 		http.Error(w, "Failed to get carrots from DB :(", http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -44,6 +45,7 @@ func (h CarrotHandler) Post(w http.ResponseWriter, r *http.Request) {
 	carrot, err := h.Repository.addCarrot()
 	if err != nil {
 		http.Error(w, "Failed to post carrot :(", http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
